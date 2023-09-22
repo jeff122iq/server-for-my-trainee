b704bdbc8d3ce43e5a842a4046c92370fe3a58c9
jeff122iq <jeff122iq@gmail.com>
2023-09-22 20:48:37 +0300

    change route

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {cardController} = require('./controllers/cardController')
 
+router.get('/', (req, res) => res.send('Hello api'));
 router.get('/getCards', cardController.getCards);
 router.post('/addCard', cardController.addCard);
 router.post('/deleteCard', cardController.deleteCard);
